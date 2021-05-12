247416e950dc29bb2bd26a525ca7c85b685d086a
unknown <antonio.salazar.emrsn@gmail.com>
2021-05-12 16:15:36 -0500

    Test for getCandidates function is now passing

--- test/VotingContract.test.js
+++ test/VotingContract.test.js
@@ -7,7 +7,8 @@ let instance;
 let accounts;
 let owner;
 let id = 0;
-let newCandidateName = 'Candidate1';
+let candidate1 = 'Candidate1';
+let candidate2 = 'Candidate2'
 let voteCount = 0;
 contract('VotingContract', async(accs) => {
     accounts = accs;
@@ -16,19 +17,28 @@ contract('VotingContract', async(accs) => {
 
 it('Can add a candidate', async() => {
     let instance = await VotingContract.deployed();
-    await instance.addCandidate(newCandidateName, {from: moderator});
+    await instance.addCandidate(candidate1, {from: moderator});
     let request = await instance.candidateLookup.call(id);
     assert.equal(request[0], id);
-    assert.equal(request[1], newCandidateName);
+    assert.equal(request[1], candidate1);
     assert.equal(request[2], voteCount);
 });
 
 it('can get candidate', async() => {
     let instance = await VotingContract.deployed();
-    await instance.addCandidate(newCandidateName, {from: moderator});
     let getCandidate = await instance.getCandidate(id);
     let getCandidateName = getCandidate[0].toString();
     let getCandidateVoteCount = getCandidate[1].toNumber();
-    assert.equal(getCandidateName, newCandidateName); //name
+    assert.equal(getCandidateName, candidate1); //name
     assert.equal(getCandidateVoteCount, voteCount); //vote count
+})
+
+it('can get all candidates', async() => {
+    let instance = await VotingContract.deployed();
+    await instance.addCandidate(candidate2, {from: moderator});
+    let getCandidates = await instance.getCandidates();
+    assert.equal(getCandidates[0][0], candidate1); // candidate1 -name
+    assert.equal(getCandidates[1][0], 0);          // candidate1 -number of votes   
+    assert.equal(getCandidates[0][1], candidate2);// candidate2 -name
+    assert.equal(getCandidates[1][1], 0)          // candidate2 -number of votes 
 })
